343abc53ceb6e8ecda9f80026b97b719cad98f55
Devanshpatel350 <devanshpatel350@gmail.com>
2026-05-18 12:33:42 -0400

    Added Day 8 canvas game, Day 9 API projects and Day 10 localStorage authentication

--- day 9/index.js
+++ day 9/index.js
@@ -1,26 +1,24 @@
 
- let inp= document.querySelector("input")
- let btn= document.querySelector("button")
- let h1= document.querySelector("h1")
- let h2= document.querySelector("h2")
- btn.addEventListener("click",()=>{
-    let apiK='9f6290d6cda9a36a63755fadee71f83d'
-    let city=inp.value
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK}`).
-    then((data)=>{
-        return data.json()
+//  let inp= document.querySelector("input")
+//  let btn= document.querySelector("button")
+//  let h1= document.querySelector("h1")
+//  let h2= document.querySelector("h2")
+//  btn.addEventListener("click",()=>{
+//     let apiK='9f6290d6cda9a36a63755fadee71f83d'
+//     let city=inp.value
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK}`).
+//     then((data)=>{
+//         return data.json()
 
-    }).then((info)=>{
-        console.log(info);
-        h2.innerText=info.name
-        h1.innerText=info.main.temp
+//     }).then((info)=>{
+//         console.log(info);
+//         h2.innerText=info.name
+//         h1.innerText=info.main.temp
         
 
-    })
+//     })
 
- })
-
-
+//  })
 
 
 
@@ -30,7 +28,9 @@
 
 
 
-//  let userN="hello"
-//  let lastName="bye"
-// //  let fullName=userN+ "       "+lastName
-// let fullName=`${userN}     ${lastName}`
+
+
+// //  let userN="hello"
+// //  let lastName="bye"
+// // //  let fullName=userN+ "       "+lastName
+// // let fullName=`${userN}     ${lastName}`
