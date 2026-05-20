8678e44656f3b69fbbe1787323d394f604ec2f4b
Devanshpatel350 <devanshpatel350@gmail.com>
2026-05-20 10:22:13 -0400

    Updated product project with add to cart page and API integration

---
+++ project/index1.js
@@ -0,0 +1,67 @@
+// // // button=document.querySelector("button");
+// // // button.addEventListener("click",()=>{
+// // //     console.log("Button Clicked");
+// // // })
+// // fetch("https://dummyjson.com/products").then((response) => {
+// //     return response.json();
+// // }).then((data) => {
+// //     let body = document.querySelector("body");
+// //     // console.log(data.products[3].brand);
+// //     data.products.map((a) => {
+// //         console.log(a.thumbnail);
+// //         body.innerHTML += <div style="border: 1px solid black;
+// //         padding: 20px;
+// //         margin: 20px;
+// //         width: 300px;
+// //         border-radius: 21px;
+// //         ">
+// //         <img src="${a.thumbnail}" width="100px"/>
+// //         <h2>${a.title}</h2>
+// //         <h2>${a.brand}</h2>
+// //         <h3>${a.price}</h3>
+// //         <button>Buy Now</button>
+// //         </div>;
+// //     })
+// // })
+
+
+
+
+// fetch("https://dummyjson.com/products").
+// then((res)=>{
+//     return res.json()
+// }).then((data)=>{
+//    let body=   document.querySelector("body")
+//     // console.log(data.products[1].brand);
+//     data.products.map((a)=>{
+//         console.log(a);
+//         body.innerHTML+=`<div style="border:1px solid black;
+//            padding:20px;
+//            margin:20px;
+//            width:300px;
+//            border-radius:21px"> 
+//            <img src="${a.
+//             thumbnail}" width="100px">
+//             <h2>  ${a.title}</h2>
+//             <h2>  ${a.brand}</h2>
+//             <button onclick="addToCart(${a.id})">add </button>
+//             </div>
+//      `
+//     })
+//     window.allP=data.products
+// })
+// console.log(window);
+// function addToCart(id){
+
+//     // console.log(id);
+//  let data=    window.allP.find((a)=>{
+//         return a.id==id
+
+
+//     })
+//     console.log(data);
+//     localStorage.setItem("item",JSON.stringify(data))
+    
+
+    
+// }
