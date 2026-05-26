a58903931eeae882c5c9457f8902751bb81805bd
Devanshpatel350 <devanshpatel350@gmail.com>
2026-05-26 13:16:22 -0400

    Added React components and updated Home page structure in Web D project

---
+++ my-react-app/src/assets/Home.jsx
@@ -0,0 +1,22 @@
+// //import React from 'react';
+// import Home from './assets/Home';
+
+// const App = () => {
+//     let user="devansh"
+//   return (
+//     <div><Home data={user} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// const Home = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.data}</h1>
+//     </div>
+//   );
+// };
+
+// export default Home;
