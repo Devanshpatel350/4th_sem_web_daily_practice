12dc7916d3f0efe1737b0473b99e0a9f6376f352
Devanshpatel350 <devanshpatel350@gmail.com>
2026-05-27 12:37:14 -0400

    Update React components and improve Home page UI

--- my-react-app/src/assets/Home.jsx
+++ my-react-app/src/assets/Home.jsx
@@ -20,3 +20,30 @@
 // };
 
 // export default Home;
+// import React from 'react'
+
+// const Home = () => {
+//   return (
+//     <div>Home</div>
+//   )
+// }
+// const New = () => {
+//     return (
+//       <div>newww fileeeee</div>
+//     )
+//   }
+//   export {New}
+
+// export default Home
+
+//import React from 'react'
+
+const Home = () => {
+    console.log("hello");
+    
+  return (
+    <div></div>
+  )
+}
+
+export default Home
