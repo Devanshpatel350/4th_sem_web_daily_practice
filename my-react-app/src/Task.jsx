ab6c83311735c53e7e1fb42809328f82b8f12d88
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-01 13:35:38 -0400

    Day 15: Implemented API data fetching, product search, sorting, and component updates

--- my-react-app/src/Task.jsx
+++ my-react-app/src/Task.jsx
@@ -1,138 +1,138 @@
 
 
-// import React, { useState } from 'react'
+// // import React, { useState } from 'react'
 
-// const Search = () => {
+// // const Search = () => {
 
+// //     let [search, SetSearch] = useState("")
+// //     console.log(search);
+
+// //     async function apiCall() {
+
+// //         if (!search.trim()) {
+// //             alert("kuch bhi")
+// //             return;
+// //         }
+
+// //         let res = await fetch(`https:dummyjson.com/products/search?q=${search}`)
+// //         let data = await res.json()
+// //         console.log(data)
+// //     }
+
+// //     return (
+        
+// //         <>
+// //             <div>Search</div>
+
+// //             <input
+// //                 onChange={(e) => SetSearch(e.target.value)}
+// //             />
+
+// //             <button onClick={apiCall}>
+// //                 Search
+// //             </button>
+// //         </>
+// //     )
+// // }
+
+// // export default Search
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react'
+
+// const Task = () => {
 //     let [search, SetSearch] = useState("")
+//     let [ApiData, SetApiData] = useState([])
+
 //     console.log(search);
 
 //     async function apiCall() {
-
+//         console.log("byeyeyeyeyeey");
 //         if (!search.trim()) {
-//             alert("kuch bhi")
+//             alert("kuch bhi nhi")
 //             return;
 //         }
-
-//         let res = await fetch(`https:dummyjson.com/products/search?q=${search}`)
+//         let res = await fetch(`https://dummyjson.com/products/search?q=${search}`)
 //         let data = await res.json()
-//         console.log(data)
+//         console.log(data);
+//         SetApiData(data.products)
+
+
 //     }
 
-//     return (
+
+
+//     useEffect(() => {
+//         fetch('https://dummyjson.com/products').then((res) => {
+//             return res.json()
+
+//         }).then((data) => {
+//             console.log(data);
+//             SetApiData(data.products)
+
+
+//         })
+//     }, [])
+
+//     function fun1(){
+//         console.log("dortt");
         
-//         <>
-//             <div>Search</div>
+//        let sortData= [...ApiData].sort((a,b)=>{
+//              return a.price-b.price
 
-//             <input
-//                 onChange={(e) => SetSearch(e.target.value)}
-//             />
+//         })
+//         SetApiData(sortData)
+//         console.log(sortData,"hello");
+        
 
-//             <button onClick={apiCall}>
-//                 Search
-//             </button>
-//         </>
+//     }
+
+
+//     function fun2(){
+//         console.log("dortt");
+        
+//        let sortData= [...ApiData].sort((a,b)=>{
+//              return b.price-a.price
+
+//         })
+//         SetApiData(sortData)
+//         console.log(sortData,"hello");
+        
+
+//     }
+
+
+
+//     return (
+//         <div>
+//             <input onChange={(e) => SetSearch(e.target.value)} />
+//             <button onClick={apiCall}>click</button>
+//             <button onClick={fun1}>low</button>
+//             <button onClick={fun2}>high</button>
+
+
+//             {
+//                 ApiData.map((a) => {
+//                     return (<div>
+//                         <img src={a.
+//                             thumbnail
+//                         } />
+//                         <h3>{a.id}</h3>
+//                         <h3>{a.price}</h3>
+
+
+//                     </div>)
+//                 })
+//             }
+//         </div>
 //     )
 // }
 
-// export default Search
-
-
-
-
-
-
-import React, { useEffect, useState } from 'react'
-
-const Task = () => {
-    let [search, SetSearch] = useState("")
-    let [ApiData, SetApiData] = useState([])
-
-    console.log(search);
-
-    async function apiCall() {
-        console.log("byeyeyeyeyeey");
-        if (!search.trim()) {
-            alert("kuch bhi nhi")
-            return;
-        }
-        let res = await fetch(`https://dummyjson.com/products/search?q=${search}`)
-        let data = await res.json()
-        console.log(data);
-        SetApiData(data.products)
-
-
-    }
-
-
-
-    useEffect(() => {
-        fetch('https://dummyjson.com/products').then((res) => {
-            return res.json()
-
-        }).then((data) => {
-            console.log(data);
-            SetApiData(data.products)
-
-
-        })
-    }, [])
-
-    function fun1(){
-        console.log("dortt");
-        
-       let sortData= [...ApiData].sort((a,b)=>{
-             return a.price-b.price
-
-        })
-        SetApiData(sortData)
-        console.log(sortData,"hello");
-        
-
-    }
-
-
-    function fun2(){
-        console.log("dortt");
-        
-       let sortData= [...ApiData].sort((a,b)=>{
-             return b.price-a.price
-
-        })
-        SetApiData(sortData)
-        console.log(sortData,"hello");
-        
-
-    }
-
-
-
-    return (
-        <div>
-            <input onChange={(e) => SetSearch(e.target.value)} />
-            <button onClick={apiCall}>click</button>
-            <button onClick={fun1}>low</button>
-            <button onClick={fun2}>high</button>
-
-
-            {
-                ApiData.map((a) => {
-                    return (<div>
-                        <img src={a.
-                            thumbnail
-                        } />
-                        <h3>{a.id}</h3>
-                        <h3>{a.price}</h3>
-
-
-                    </div>)
-                })
-            }
-        </div>
-    )
-}
-
-export default Task
+// export default Task
 
 
 
