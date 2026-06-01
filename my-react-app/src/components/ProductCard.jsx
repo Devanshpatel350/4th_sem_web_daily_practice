61313fdc692b7efb1d919c0c3172bd24394762b7
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-01 13:30:17 -0400

    Day 15: Implemented API data fetching, product search, sorting, and component updates

---
+++ my-react-app/src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+function ProductCard({ product }) {
+  return (
+    <div className="card">
+      <img src={product.thumbnail} alt={product.title} />
+
+      <h3>{product.title}</h3>
+
+      <p>{product.brand}</p>
+
+      <p className="price">₹ {product.price}</p>
+    </div>
+  );
+}
+
+export default ProductCard;
