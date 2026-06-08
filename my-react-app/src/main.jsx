bb1c293d2e6b1067fb1bbf1e1a1f009b71bced94
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-08 11:37:22 -0400

    feat: add React Router navigation, product API integration, and cart functionality

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,26 +1,26 @@
+// // import { StrictMode } from 'react'
+// // import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// // import App from './App.jsx'
+
+// // createRoot(document.getElementById('root')).render(
+// //   <StrictMode>
+// //     <App />
+// //   </StrictMode>,
+// // )
+
+
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
-// import './index.css'
+// // import './index.css'
 // import App from './App.jsx'
-
+// import Context from './Context.jsx'
+// import { BrowserRouter } from 'react-router-dom'
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
+//    <BrowserRouter> 
 //     <App />
+//     </BrowserRouter>
+
 //   </StrictMode>,
 // )
-
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-import Context from './Context.jsx'
-import { BrowserRouter } from 'react-router-dom'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <BrowserRouter> 
-    <App />
-    </BrowserRouter>
-
-  </StrictMode>,
-)
