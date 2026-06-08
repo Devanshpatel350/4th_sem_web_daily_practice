a02f24c58b727bbd292649e7fa7e11cbebe80c04
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-08 11:38:08 -0400

    feat: add React Router navigation, product API integration, and cart functionality

--- my-react-app/src/context.jsx
+++ my-react-app/src/context.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+// import { createContext } from "react";
 
- let Context=   createContext()
- export default Context
+//  let Context=   createContext()
+//  export default Context
