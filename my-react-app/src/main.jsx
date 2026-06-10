e622519dca884aa2163f45579ff0e1556690926c
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-10 13:24:27 -0400

    feat: implement Context API and useReducer for Todo management

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,47 +1,47 @@
+// // // import { StrictMode } from 'react'
+// // // import { createRoot } from 'react-dom/client'
+// // // import './index.css'
+// // // import App from './App.jsx'
+
+// // // createRoot(document.getElementById('root')).render(
+// // //   <StrictMode>
+// // //     <App />
+// // //   </StrictMode>,
+// // // )
+
+
 // // import { StrictMode } from 'react'
 // // import { createRoot } from 'react-dom/client'
-// // import './index.css'
+// // // import './index.css'
 // // import App from './App.jsx'
-
+// // import Context from './Context.jsx'
+// // import { BrowserRouter } from 'react-router-dom'
 // // createRoot(document.getElementById('root')).render(
 // //   <StrictMode>
+// //    <BrowserRouter> 
 // //     <App />
+// //     </BrowserRouter>
+
 // //   </StrictMode>,
 // // )
+
 
 
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 // // import './index.css'
 // import App from './App.jsx'
-// import Context from './Context.jsx'
+
 // import { BrowserRouter } from 'react-router-dom'
+// import Context from './Context.jsx'
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
-//    <BrowserRouter> 
+//     <Context>   
+
 //     <App />
-//     </BrowserRouter>
-
-//   </StrictMode>,
-// )
-
-
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-
-import { BrowserRouter } from 'react-router-dom'
-import Context from './Context.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Context>   
-
-    <App />
-    </Context>
+//     </Context>
  
 
 
-  </StrictMode>,
-)
+//   </StrictMode>,
+// )
