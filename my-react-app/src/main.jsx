498b77c6d07cc8bb5de231aff91533b89e71c6e8
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-12 14:12:32 -0400

    Update React project with Context API, useReducer, routing, todo CRUD, and custom hook examples

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,29 +1,53 @@
+// // // // import { StrictMode } from 'react'
+// // // // import { createRoot } from 'react-dom/client'
+// // // // import './index.css'
+// // // // import App from './App.jsx'
+
+// // // // createRoot(document.getElementById('root')).render(
+// // // //   <StrictMode>
+// // // //     <App />
+// // // //   </StrictMode>,
+// // // // )
+
+
 // // // import { StrictMode } from 'react'
 // // // import { createRoot } from 'react-dom/client'
-// // // import './index.css'
+// // // // import './index.css'
 // // // import App from './App.jsx'
-
+// // // import Context from './Context.jsx'
+// // // import { BrowserRouter } from 'react-router-dom'
 // // // createRoot(document.getElementById('root')).render(
 // // //   <StrictMode>
+// // //    <BrowserRouter> 
 // // //     <App />
+// // //     </BrowserRouter>
+
 // // //   </StrictMode>,
 // // // )
+
 
 
 // // import { StrictMode } from 'react'
 // // import { createRoot } from 'react-dom/client'
 // // // import './index.css'
 // // import App from './App.jsx'
-// // import Context from './Context.jsx'
+
 // // import { BrowserRouter } from 'react-router-dom'
+// // import Context from './Context.jsx'
 // // createRoot(document.getElementById('root')).render(
 // //   <StrictMode>
-// //    <BrowserRouter> 
+// //     <Context>   
+
 // //     <App />
-// //     </BrowserRouter>
+// //     </Context>
+ 
+
 
 // //   </StrictMode>,
 // // )
+
+
+
 
 
 
@@ -31,45 +55,21 @@
 // import { createRoot } from 'react-dom/client'
 // // import './index.css'
 // import App from './App.jsx'
+// import C, { Context } from './Context.jsx'
 
-// import { BrowserRouter } from 'react-router-dom'
-// import Context from './Context.jsx'
+// // import Context from './Context.jsx'
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
-//     <Context>   
-
+ 
+// <Context> 
+  
 //     <App />
 //     </Context>
+  
+  
+ 
  
 
 
 //   </StrictMode>,
 // )
-
-
-
-
-
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-import C, { Context } from './Context.jsx'
-
-// import Context from './Context.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
- 
-<Context> 
-  
-    <App />
-    </Context>
-  
-  
- 
- 
-
-
-  </StrictMode>,
-)
