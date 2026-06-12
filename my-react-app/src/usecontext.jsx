1b452b3a9444b7512c28b04e417ee9da7c557dbf
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-12 14:16:14 -0400

    Update React project with Context API, useReducer, routing, todo CRUD, and custom hook examples

--- my-react-app/src/usecontext.jsx
+++ my-react-app/src/usecontext.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function useCounter(data=0){
-  let [count,SetCount]=  useState(data)
-  const inc=()=>SetCount(count+1)
-  const dec=()=>SetCount(count-1)
-  const reset=()=>SetCount(0)
-  return {count,inc,dec,reset}
+// function useCounter(data=0){
+//   let [count,SetCount]=  useState(data)
+//   const inc=()=>SetCount(count+1)
+//   const dec=()=>SetCount(count-1)
+//   const reset=()=>SetCount(0)
+//   return {count,inc,dec,reset}
 
 
-}
-export default useCounter
+// }
+// export default useCounter
