e219bb2c011450b55a644ce409a6a92ee1609588
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-15 12:51:47 -0400

    feat: implement user list and dynamic profile routing

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,29 +1,53 @@
+// // // // // // import { StrictMode } from 'react'
+// // // // // // import { createRoot } from 'react-dom/client'
+// // // // // // import './index.css'
+// // // // // // import App from './App.jsx'
+
+// // // // // // createRoot(document.getElementById('root')).render(
+// // // // // //   <StrictMode>
+// // // // // //     <App />
+// // // // // //   </StrictMode>,
+// // // // // // )
+
+
 // // // // // import { StrictMode } from 'react'
 // // // // // import { createRoot } from 'react-dom/client'
-// // // // // import './index.css'
+// // // // // // import './index.css'
 // // // // // import App from './App.jsx'
-
+// // // // // import Context from './Context.jsx'
+// // // // // import { BrowserRouter } from 'react-router-dom'
 // // // // // createRoot(document.getElementById('root')).render(
 // // // // //   <StrictMode>
+// // // // //    <BrowserRouter> 
 // // // // //     <App />
+// // // // //     </BrowserRouter>
+
 // // // // //   </StrictMode>,
 // // // // // )
+
 
 
 // // // // import { StrictMode } from 'react'
 // // // // import { createRoot } from 'react-dom/client'
 // // // // // import './index.css'
 // // // // import App from './App.jsx'
-// // // // import Context from './Context.jsx'
+
 // // // // import { BrowserRouter } from 'react-router-dom'
+// // // // import Context from './Context.jsx'
 // // // // createRoot(document.getElementById('root')).render(
 // // // //   <StrictMode>
-// // // //    <BrowserRouter> 
+// // // //     <Context>   
+
 // // // //     <App />
-// // // //     </BrowserRouter>
+// // // //     </Context>
+ 
+
 
 // // // //   </StrictMode>,
 // // // // )
+
+
+
 
 
 
@@ -31,40 +55,40 @@
 // // // import { createRoot } from 'react-dom/client'
 // // // // import './index.css'
 // // // import App from './App.jsx'
+// // // import C, { Context } from './Context.jsx'
 
-// // // import { BrowserRouter } from 'react-router-dom'
-// // // import Context from './Context.jsx'
+// // // // import Context from './Context.jsx'
 // // // createRoot(document.getElementById('root')).render(
 // // //   <StrictMode>
-// // //     <Context>   
-
+ 
+// // // <Context> 
+  
 // // //     <App />
 // // //     </Context>
+  
+  
+ 
  
 
 
 // // //   </StrictMode>,
 // // // )
 
-
-
-
-
-
 // // import { StrictMode } from 'react'
 // // import { createRoot } from 'react-dom/client'
-// // // import './index.css'
+// //  import './index.css'
 // // import App from './App.jsx'
-// // import C, { Context } from './Context.jsx'
+// // import {BrowserRouter}   from 'react-router-dom'
 
 // // // import Context from './Context.jsx'
 // // createRoot(document.getElementById('root')).render(
 // //   <StrictMode>
  
-// // <Context> 
-  
+
+// //   <BrowserRouter> 
 // //     <App />
-// //     </Context>
+// //     </BrowserRouter>
+
   
   
  
@@ -73,10 +97,9 @@
 
 // //   </StrictMode>,
 // // )
-
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
-//  import './index.css'
+// import './index.css'
 // import App from './App.jsx'
 // import {BrowserRouter}   from 'react-router-dom'
 
@@ -84,7 +107,6 @@
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
  
-
 //   <BrowserRouter> 
 //     <App />
 //     </BrowserRouter>
@@ -97,25 +119,3 @@
 
 //   </StrictMode>,
 // )
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter}   from 'react-router-dom'
-
-// import Context from './Context.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
- 
-  <BrowserRouter> 
-    <App />
-    </BrowserRouter>
-
-  
-  
- 
- 
-
-
-  </StrictMode>,
-)
